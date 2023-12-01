23e0ed79f627ae228b9f2766d6dc0a45f8c30ed9
MauricioPerezCaram <perezcarammauricio@gmail.com>
2023-12-01 17:38:31 -0300

    falta responsive

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
+import clear_icon from "./clear.png";
+import cloud_icon from "./cloud.png";
+import drizzle_icon from "./drizzle.png";
+import rain_icon from "./rain.png";
+import snow_icon from "./snow.png";
 
 const WeatherApp = () => {
   let api_key = "2a44a42c2daa7f56e24859f15616908c";
 
-  const [wicon, setWicon] = useState();
+  const [wicon, setWicon] = useState(snow_icon);
 
   const search = async () => {
     const element = document.getElementsByClassName("cityInput");
@@ -13,9 +18,11 @@ const WeatherApp = () => {
 
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
 
+    let data;
+
     try {
       let response = await fetch(url);
-      let data = await response.json();
+      data = await response.json();
 
       const humidityElements =
         document.getElementsByClassName("humidity-percent");
@@ -42,6 +49,51 @@ const WeatherApp = () => {
       }
     } catch (error) {
       console.error("Error fetching weather data:", error);
+      // Mostrar un mensaje al usuario cuando la ubicación no se encuentra
+      alert("Location not found");
+      return;
+    }
+
+    // Verificar si data está definido y contiene la información esperada
+    if (data && data.weather && data.weather.length > 0) {
+      if (data.weather[0].icon === "01d" || data.weather[0].icon === "01n") {
+        setWicon(clear_icon);
+      } else if (
+        data.weather[0].icon === "02d" ||
+        data.weather[0].icon === "02n"
+      ) {
+        setWicon(cloud_icon);
+      } else if (
+        data.weather[0].icon === "03d" ||
+        data.weather[0].icon === "03n"
+      ) {
+        setWicon(drizzle_icon);
+      } else if (
+        data.weather[0].icon === "04d" ||
+        data.weather[0].icon === "04n"
+      ) {
+        setWicon(drizzle_icon);
+      } else if (
+        data.weather[0].icon === "09d" ||
+        data.weather[0].icon === "09n"
+      ) {
+        setWicon(rain_icon);
+      } else if (
+        data.weather[0].icon === "10d" ||
+        data.weather[0].icon === "10n"
+      ) {
+        setWicon(rain_icon);
+      } else if (
+        data.weather[0].icon === "13d" ||
+        data.weather[0].icon === "13n"
+      ) {
+        setWicon(snow_icon);
+      } else {
+        setWicon(clear_icon);
+      }
+    } else {
+      // Mostrar un mensaje si data no contiene la información esperada
+      alert("Location not found");
     }
   };
 
@@ -59,23 +111,23 @@ const WeatherApp = () => {
         </div>
       </div>
       <div className="weather-image">
-        <img src="/Assets/cloud.png" alt="Cloud" className="icon" />
+        <img src={wicon} alt="" />
       </div>
-      <div className="weather-temp">24° C</div>
-      <div className="weather-location">Mendoza</div>
+      <div className="weather-temp"></div>
+      <div className="weather-location"></div>
       <div className="data-container">
         <div className="element">
           <img src="/Assets/humidity.png" alt="humidity" className="icon" />
           <div className="data">
-            <div className="humidity-percent">64 %</div>
+            <div className="humidity-percent"></div>
             <div className="text">Humidity</div>
           </div>
         </div>
         <div className="element">
           <img src="/Assets/wind.png" alt="wind" className="icon" />
           <div className="data">
-            <div className="wind-rate">20 km/h</div>
-            <div className="text">Wind Speed</div>
+            <div className="wind-rate"></div>
+            <div className="text"></div>
           </div>
         </div>
       </div>
